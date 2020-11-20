2aaeb27a107aeacedaf988d0b09ea2eaa4f5cce5
evgeny.g <evgeniy.g@fintuity.com>
2020-11-20 17:04:19 +0300

    add app header

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -7,6 +7,12 @@ const AppBar = () => {
   return (
     <div className="AppBar">
       <SVG src={logo} className="AppBar__Logo"></SVG>
+      <ul className="AppBar__Nav">
+        <li className="AppBar__NavItem">Сервисы</li>
+        <li className="AppBar__NavItem">Задачи</li>
+        <li className="AppBar__NavItem">События</li>
+      </ul>
+      <div className="AppBar__Toolbar"></div>
     </div>
   )
 }
